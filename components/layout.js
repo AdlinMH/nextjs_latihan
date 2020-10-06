2df763a924318244570f5d9933c90d3f98c79543
AdlinM <adlin@rushowl.sg>
2020-10-06 11:20:07 +0700

    feat: init and basic code
    - Create a Next.js App
    - Navigate between Pages
    - Assets, Metadata, and CSS

---
+++ components/layout.js
@@ -0,0 +1,69 @@
+import Head from 'next/head'
+import Link from 'next/Link'
+
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+
+const name = "Adlin Mohammad H"
+export const siteTitle = 'Next.js Sample Website'
+
+function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <img
+              src="/images/profile.jpg"
+              className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
+              alt={name}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <a>
+                <img
+                  src="/images/profile.jpg"
+                  className={`${styles.headerImage} ${utilStyles.borderCircle}`}
+                  alt={name}
+                />
+              </a>
+              <h2 className={utilStyles.headingLg}>
+                <Link href="/">
+                  <a className={utilStyles.colorInherit}>{name}</a>
+                </Link>
+              </h2>
+            </Link>
+          </>
+        )}
+      </header>
+
+      <main>{children}</main>
+
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">
+            <a>Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>)
+}
+
+export default Layout
